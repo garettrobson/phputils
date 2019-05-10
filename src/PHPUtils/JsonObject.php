3a1f3b14215126f9d5f4bdb54c326cd104f2b8b6
Garett Robson <info@garettrobson.co.uk>
2019-05-10 20:07:11 +0100

    Added get function; added documentation; supporting tests tightened up.

--- src/PHPUtils/JsonObject.php
+++ src/PHPUtils/JsonObject.php
@@ -1,19 +1,36 @@
 <?php
+/**
+* @author Garett Robson <info@garettrobson.co.uk
+*/
+
 declare(strict_types=1);
 
 namespace PHPUtils;
 
 use ArrayAccess;
 use stdClass;
+use object;
+use string;
 
+/**
+*
+* Class to offer convenience functions for common json_decode'ed objects.
+*
+*/
 class JsonObject
 {
-    public static function combine(stdClass $destination, stdClass ...$sources)
+    /**
+    *
+    * Combine a variable number soruce stdClass and merges them into the destination stdClass.
+    *
+    * @param object $destination The objects to merge into a new object.
+    * @param object ...$sources The objects to merge into a new object.
+    * @return object The new $destination object, with all merged properties.
+    */
+    public static function combine(object $destination, object ...$sources)
     {
-        foreach($sources as $source)
-        {
-            foreach($source as $property => $value)
-            {
+        foreach ($sources as $source) {
+            foreach ($source as $property => $value) {
                 $valueType = gettype($value);
                 if (
                     $valueType === 'array' &&
@@ -33,8 +50,58 @@ class JsonObject
         return $destination;
     }
 
-    public static function merge(stdClass ...$sources)
+    /**
+    *
+    * Combine a variable number of stdClass objects into a new object.
+    *
+    * @param object ...$sources The objects to merge into a new object.
+    * @return stdClass The new object.
+    */
+    public static function merge(object ...$sources)
     {
         return static::combine(new stdClass, ...$soruces);
+    }
+
+    /**
+    *
+    * Uses a string-based address to retrieve values from a nested object
+    *
+    * Example:
+    *  $o = { an: { example: { address: "value" } } }
+    *  get($o, "an.example.address") // would return "value"
+    *  get($o, "a.bad.address") // would return null
+    *  get($o, "a.bad.address", "default") // would return "default"
+    *  get($o, "an/example/address", "default", "/") // would return "value"
+    *  get($o, "a/bad/address", null, "/") // would return null
+    *
+    * @param object $source The object to search.
+    * @return string $address The address of the value to return.
+    * @return mixed $default The value to return when the value is not found.
+    * @return string $delimiter The delimiter for the address.
+    */
+    public static function get(object $source, string $address, $default = null, $delimiter = '.')
+    {
+        $parts = explode($delimiter, $address);
+        $container = $source;
+        while ($key = array_shift($parts)) {
+            $type = gettype($container);
+            switch ($type) {
+                case 'array':
+                    if (!array_key_exists($key, $container)) {
+                        return $default;
+                    }
+                    $container = $contianer[$key];
+                    break;
+                case 'object':
+                    if (!property_exists($container, $key)) {
+                        return $default;
+                    }
+                    $container = $container->$key;
+                    break;
+                default:
+                    return $default;
+            }
+        }
+        return $container;
     }
 }
--- tests/PHPUtils/JsonObjectTest.php
+++ tests/PHPUtils/JsonObjectTest.php
@@ -8,101 +8,104 @@ final class EmailTest extends TestCase
 {
     public function testSimpleCombineObjects(): void
     {
-
         $destination = json_decode('{"foo": "bar"}');
         $source = json_decode('{"baz": "qux"}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('foo', $destination);
-        $this->assertSame($destination->foo, "bar");
-
         $this->assertObjectHasAttribute('baz', $destination);
+
+        $this->assertSame($destination->foo, "bar");
         $this->assertSame($destination->baz, "qux");
     }
 
-    public function testSimpleOverwrite(): void
+    public function testSimpleOverwriteCombine(): void
     {
-
         $destination = json_decode('{"overwrite":"original"}');
         $source = json_decode('{"overwrite": "overwriten"}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('overwrite', $destination);
+
         $this->assertSame($destination->overwrite, "overwriten");
     }
 
-    public function testSimpleArrayMerge(): void
+    public function testSimpleArrayCombine(): void
     {
-
         $destination = json_decode('{"merge":["foo"]}');
         $source = json_decode('{"merge": ["bar"]}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('merge', $destination);
+
         $this->assertSame($destination->merge, ['foo', 'bar']);
     }
 
     public function testDeepCombineObjects(): void
     {
-
         $destination = json_decode('{"key": {"foo": "bar"}}');
         $source = json_decode('{"key": {"baz": "qux"}}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('key', $destination);
-
         $this->assertObjectHasAttribute('foo', $destination->key);
-        $this->assertSame($destination->key->foo, "bar");
-
         $this->assertObjectHasAttribute('baz', $destination->key);
-        $this->assertSame($destination->key->baz, "qux");
 
+        $this->assertSame($destination->key->foo, "bar");
+        $this->assertSame($destination->key->baz, "qux");
     }
 
     public function testCreateCombineObjects(): void
     {
-
         $destination = json_decode('{}');
         $source = json_decode('{"key": {"baz": "qux"}}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('key', $destination);
-
         $this->assertObjectHasAttribute('baz', $destination->key);
-        $this->assertSame($destination->key->baz, "qux");
 
+        $this->assertSame($destination->key->baz, "qux");
     }
 
-    public function testDeepOverwrite(): void
+    public function testDeepOverwriteCombine(): void
     {
-
         $destination = json_decode('{"key": {"overwrite":"original"}}');
         $source = json_decode('{"key": {"overwrite": "overwriten"}}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('key', $destination);
-
         $this->assertObjectHasAttribute('overwrite', $destination->key);
+
         $this->assertSame($destination->key->overwrite, "overwriten");
     }
 
-    public function testDeepArrayMerge(): void
+    public function testDeepArrayCombine(): void
     {
-
         $destination = json_decode('{"key": {"merge":["foo"]}}');
         $source = json_decode('{"key": {"merge": ["bar"]}}');
 
         JsonObject::combine($destination, $source);
 
         $this->assertObjectHasAttribute('key', $destination);
-
         $this->assertObjectHasAttribute('merge', $destination->key);
+
         $this->assertSame($destination->key->merge, ['foo', 'bar']);
+    }
+
+    public function testValue(): void
+    {
+        $search = json_decode('{"foo": {"bar": "baz", "qux": ["quux", "quuz", "corge"]}}');
+
+        $value = JsonObject::get($search, "foo.bar");
+        $this->assertSame($value, "baz");
+
+        $value = JsonObject::get($search, "foo.qux");
+        $this->assertSame($value, ["quux", "quuz", "corge"]);
     }
 }
